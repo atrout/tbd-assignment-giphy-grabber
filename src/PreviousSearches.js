a5e88cf291f9ab54d7d6cb50db0c522b338993fa
atrout <aarmentrout@meetup.com>
2023-02-19 22:37:12 -0500

    remove whitespace

--- src/PreviousSearches.js
+++ src/PreviousSearches.js
@@ -1,13 +1,13 @@
 
 const PreviousSearches = ({previousSearches, setSearch}) => {
-  const listItems = previousSearches?.map( (prev, index) => 
+  const listItems = previousSearches?.map( (prev, index) =>
     <li key={index}><button onClick={() => setSearch(prev === 'trending' ? '' : prev)}>{prev}</button></li>
   );
   return (
     <div className='previous-searches'>
       <p>Previous <br/>Searches:</p>
       <ul>
-        {listItems} 
+        {listItems}
       </ul>
     </div>
   );
