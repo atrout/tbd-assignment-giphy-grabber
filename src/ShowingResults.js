f2ddd7bab1ff14c67a6271fed63e546a09783363
atrout <a.sigrid.trout@gmail.com>
2023-02-20 18:25:07 -0500

    moved components into subfolder; renamed GiphyGrabber back to App; rearranged css

--- src/ShowingResults.js
+++
@@ -1,9 +0,0 @@
-const ShowingResults = ({searchKey}) => {
-  return (
-    <>
-      <p>Showing results for: <span className='emphasis'>{searchKey}</span></p>
-    </>
-  );
-}
-
-export default ShowingResults;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GiphyGrabber from './GiphyGrabber';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GiphyGrabber />
+    <App />
   </React.StrictMode>
 );
