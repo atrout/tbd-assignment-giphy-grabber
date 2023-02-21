59d410ee2dbf661d7a7fc522ee9469260aa78162
atrout <a.sigrid.trout@gmail.com>
2023-02-20 20:49:52 -0500

    added search components test

--- src/components/SearchComponents.test.js
+++ src/components/SearchComponents.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import { SearchContextManager } from '@giphy/react-components';
 import SearchComponents from './SearchComponents';
 
-test('renders Giphy Grabber header', () => {
-  // render(<App />);
-  // const headerElement = screen.getByText(/GIPHY GRABBER/i);
-  // expect(headerElement).toBeInTheDocument();
+test('renders Search Components', () => {
+  const { container } = render(
+    <SearchContextManager>
+      <SearchComponents />
+    </SearchContextManager>);
+  expect(container.getElementsByClassName('giphy-search-bar').length).toBe(1);
+  expect(container.getElementsByClassName('previous-searches').length).toBe(1);
+  expect(container.getElementsByClassName('giphy-grid').length).toBe(1);
+
+  const searchTermElements = screen.getAllByText(/trending/i);
+  expect(searchTermElements.length).toBe(2);
 });
